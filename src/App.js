de5c84f526e2fbea5ef1c255fab692261b834f0b
Mike Baranski <baranski@us.ibm.com>
2018-05-05 14:09:50 -0400

    .

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import GeoCoder from 'react-google-geocoder'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <GeoCoder apiKey="AIzaSyCqJeH1bp062RgnAHmlrxVa-Y_dq1PFU6g" showFormTitle={true}/>
+      </div>
+    );
+  }
+}
+
+export default App;
