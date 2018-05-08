8b1d09f166f7be609c6add089a2c0a06eb9d547a
Mike Baranski <baranski@us.ibm.com>
2018-05-07 20:11:44 -0400

    Updating dependencies

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React, {Component} from 'react';
 import './App.css';
 import GeoCoder from 'react-google-geocoder'
 
+import PrismCode from 'react-prism'
+
+const customInput = ({searching, address, onChange}) => {
+    return (
+        <input disabled={searching}
+               value={address}
+               onChange={(event) => onChange(event.target.value)}/>
+    )
+}
+
 class App extends Component {
     render() {
-        const apiKey = "AIzaSyCtjPHjqXV5TqB_WFIUnk4gpjbmCldNrEU"
+        const apiKey = "AIzaSyCqJeH1bp062RgnAHmlrxVa-Y_dq1PFU6g"
         const outerDivStyle = {
-            float: "left",
             padding: "10px",
             margin: "10px",
-            height: "300px",
             backgroundColor: "azure",
+            margin: "0 auto",
+            width: "600px;",
+            float: "left",
+            padding: "1em",
         }
         const innerDivStyle = {
             border: "1px dashed black",
@@ -30,17 +42,45 @@ class App extends Component {
                 <div style={outerDivStyle}>
                     <h3>Default</h3>
                     <pre>
+                    <PrismCode className="language-javascript">
                         {'<GeoCoder apiKey={apiKey}/>'}
+                    </PrismCode>
                     </pre>
                     <div style={innerDivStyle}>
                         <GeoCoder apiKey={apiKey}/>
                     </div>
                 </div>
+                <div style={outerDivStyle}>
+                    <h3>Custom Input</h3>
+                    <pre>
+                    <PrismCode className="language-javascript">
+                        {`
+const customInput = ({searching, address, onChange}) => {
+    return (
+        <input disabled={searching}
+               value={address}
+               onChange={(event) => onChange(event.target.value)}/>
+    )
+}
 
+
+<GeoCoder apiKey={apiKey} input={customInput}/>
+                        `}
+
+                    </PrismCode>
+                    </pre>
+                    <div style={innerDivStyle}>
+                        <GeoCoder apiKey={apiKey}
+                                  input={customInput}
+                        />
+                    </div>
+                </div>
                 <div style={outerDivStyle}>
                     <h3>No Title</h3>
                     <pre>
+                        <PrismCode className="language-javascript">
                         {'<GeoCoder apiKey={apiKey} formTitle={false}/>'}
+                        </PrismCode>
                     </pre>
                     <div style={innerDivStyle}>
                         <GeoCoder apiKey={apiKey} formTitle={false}/>
@@ -50,15 +90,18 @@ class App extends Component {
                 <div style={outerDivStyle}>
                     <h3>Custom Label</h3>
                     <pre>
+                    <PrismCode className="language-javascript">
                         {'<GeoCoder fieldLabel={"Custom Label"}/>'}
+                    </PrismCode>
                     </pre>
                     <div style={innerDivStyle}>
                         <GeoCoder apiKey={apiKey} fieldLabel={"Custom Label"}/>
                     </div>
                 </div>
-            </div>
-        );
-    }
-}
 
-export default App;
+            </div>
+    );
+    }
+    }
+
+    export default App;
